b54cf01b3284f7448ce0b93d7047073a675f16fa
akaribrahim <bilalcanakar@gmail.com>
2020-04-16 16:11:05 +0300

    Categories database connection

--- app/src/main/java/com/zurefaseverler/kithub/Kategoriler.java
+++ app/src/main/java/com/zurefaseverler/kithub/Kategoriler.java
@@ -4,10 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ExpandableListView;
 import android.widget.ImageButton;
 import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Iterator;
 
 
 
@@ -17,7 +33,12 @@ import android.widget.TextView;
 
 public class Kategoriler extends AppCompatActivity implements View.OnClickListener{
 
-private int son_Expand_group = -1;
+    private int son_Expand_group = -1;
+
+    private ArrayList<String> bookCategories;
+    private ArrayList<String[]> bookTypes;
+    private RequestQueue mQueue;
+    private ExpandableListView kategoriler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,10 +48,13 @@ private int son_Expand_group = -1;
         ImageButton go_back = findViewById(R.id.go_back);
         go_back.setOnClickListener(this);
 
+        bookCategories = new ArrayList<>();
+        bookTypes = new ArrayList<>();
 
+        kategoriler = (ExpandableListView) findViewById(R.id.acilirKategoriler);
 
-        final ExpandableListView kategoriler = (ExpandableListView) findViewById(R.id.acilirKategoriler);
-        kategoriler.setAdapter( new adapter_kategoriler(this));
+        fillArrays();
+        adapter_kategoriler a = new adapter_kategoriler(this,bookCategories,bookTypes);
 
 
         kategoriler.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
@@ -57,6 +81,45 @@ private int son_Expand_group = -1;
                 return false;
             }
         });
+    }
+
+    public void fillArrays(){
+        mQueue = Volley.newRequestQueue(this);
+        String url = "http://192.168.1.36/KitHub/categories.php";
+        StringRequest request = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            Iterator<String> keys = jsonObject.keys();
+                            while(keys.hasNext()){
+                                bookCategories.add(keys.next());
+                            }
+                            for(int i =0 ;i<bookCategories.size();i++){
+                                JSONArray jsonArray = jsonObject.getJSONArray(bookCategories.get(i));
+                                String[] arr = new String[jsonArray.length()];
+                                for(int j=0;j<jsonArray.length();j++){
+                                    arr[j] = jsonArray.getString(j);
+                                }
+                                bookTypes.add(arr);
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        adapter_kategoriler a = new adapter_kategoriler(Kategoriler.this,bookCategories,bookTypes);
+                        kategoriler.setAdapter(a);
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+                });
+        mQueue.add(request);
     }
 
     @Override
--- app/src/main/java/com/zurefaseverler/kithub/adapter_kategoriler.java
+++ app/src/main/java/com/zurefaseverler/kithub/adapter_kategoriler.java
@@ -3,40 +3,39 @@ package com.zurefaseverler.kithub;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class adapter_kategoriler extends BaseExpandableListAdapter {
 
     // kategoriler veri tabani geldiginde oraya aktarilacak
     // ayrica adminin yeni kategori ekleyebilmesi lazim
-    private String[] kitap_Kategorileri = {"Edebiyat", "Çocuk ve Gençlik", "Eğitim ve Gelişim", "Araştırma ve Tarih", "Din Tasavvuf", "Yabancı Dil", "Ders ve Sınav", "Sanat - Tasarım", "Felsefe", "Bilim", "Mizah",
-            "Sesli kitap"};
-    String[] yazi = {"Roman", "Siir", "Hikaye", "Deneme", "Biyografi", "Polisiye", "Elestiri", "Cizgi Roman"};
-
-    private String[][] alt_kategoriler = {{"Roman", "Siir", "Deneme", "Öykü", "Edebiyat İnceleme", "Biyografi & Oto biyografi", "Anlatı", "Dergi"},     // Edebiyat
-            {"Okul Çağı 6-10 Yaş", "Okul Öncesi 6 Ay-5 Yaş", "Gençlik 10+ Yaş", "Okul Kitapları"},                                                      // Çocuk ve Gençlik
-            {"İş - Hukuk", "Kişisel Gelişim", "Aile Çocuk", "Psikoloji", "Eğitim", "Gezi", "Sağlık", "Etimoloji","Sözlük"},                             // Eğitim ve Gelişim
-            {"Tarih", "Politika", "Sosyoloji"},                                                                                                         // Tarih
-            {"İslamiyet", "Tasavvuf", "Din", "Hristiyanlık", "Diğer İnançlar", "Musevilik"},                                                            // Din Tasavvuf
-            {"Dil", "Edebiyat ve Roman", "Çocuk ve Genç", "Çizgi Roman"},                                                                               // Yabancı Dil
-            {"Üniversite Ders Kitapları", "Sınav Kitapları", "Ders Kitapları/Testler"},                                                                 // Ders ve Sınav
-            {"Tiyatro", "Müzik", "Sinema", "Sanat Tarihi ve Kuramı", "Mimari", "Senaryo"},                                                              // Sanat - Tasarım
-            {"Felsefe Bilimi", "Felsefeciler", "Felsefi Romanlar"},                                                                                     // Felsefe
-            {"Popüler Bilim", "Matematik", "Mühendislik", "Bilim Tarihi ve Felsefesi", "Arkeoloji", "Antropoloji", "Biyoloji", "Fizik",                 // Bilim
-                    "Coğrafya","Bilim İnsan","Jeoloji","Kimya","Astronomi","Zooloji"},
-            {"Mizah Ramanı-Öykü", "Karikatür", "Fıkra"},                                                                                                 // Mizah
-            {"Sesli Çocuk Kitabı"}                                                                                                                       // Sesli kitap
-    };
-
-
-
+    ArrayList<String> bookCategories;
+    ArrayList<String[]> bookTypes;
     private Context c;
 
-    public adapter_kategoriler(Context c){
+
+    public adapter_kategoriler(Context c,ArrayList<String> bookCategories, ArrayList<String[]> bookTypes){
         this.c = c;
+        this.bookCategories = bookCategories;
+        this.bookTypes = bookTypes;
     };
 
     @Override
@@ -46,12 +45,12 @@ public class adapter_kategoriler extends BaseExpandableListAdapter {
 
     @Override
     public int getGroupCount() {
-        return kitap_Kategorileri.length;
+        return bookCategories.size();
     }
 
     @Override
     public int getChildrenCount(int groupPosition) {
-        return alt_kategoriler[groupPosition].length;
+        return bookTypes.get(groupPosition).length;
     }
 
     @Override
@@ -79,7 +78,7 @@ public class adapter_kategoriler extends BaseExpandableListAdapter {
     @Override
     public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
         TextView tv_kitap_kategorileri = new TextView(c);
-        tv_kitap_kategorileri.setText(kitap_Kategorileri[groupPosition]);
+        tv_kitap_kategorileri.setText(bookCategories.get(groupPosition));
         tv_kitap_kategorileri.setTextSize(20);
         tv_kitap_kategorileri.setPadding(140,10,10, 10);
         tv_kitap_kategorileri.setTextColor(Color.parseColor("#ff5e3a"));
@@ -89,7 +88,7 @@ public class adapter_kategoriler extends BaseExpandableListAdapter {
     @Override
     public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
         TextView tv_alt_kategorileri = new TextView(c);
-        tv_alt_kategorileri.setText(alt_kategoriler[groupPosition][childPosition]);
+        tv_alt_kategorileri.setText(bookTypes.get(groupPosition)[childPosition]);
         tv_alt_kategorileri.setTextSize(16);
         tv_alt_kategorileri.setPadding(170,10,10, 10);
         tv_alt_kategorileri.setTextColor(Color.parseColor("#272e4f"));
