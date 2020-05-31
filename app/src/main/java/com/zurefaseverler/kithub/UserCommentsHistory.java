588031d9613794ac815f094a487c76de02dc5621
Oner Ince <onerince@hacettepe.edu.tr>
2020-05-31 15:02:50 +0300

    user comment history database connection

--- app/src/main/java/com/zurefaseverler/kithub/UserCommentsHistory.java
+++ app/src/main/java/com/zurefaseverler/kithub/UserCommentsHistory.java
@@ -4,35 +4,84 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class UserCommentsHistory extends AppCompatActivity {
 
     List<OldCommentsObj> list = new ArrayList<>();
+    private OldCommentsObj comment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_comments_history);
 
-        fill();
-        RecyclerView view = findViewById(R.id.old_comments);
-        OldCommentAdapter adapter = new OldCommentAdapter(this, list);
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
-        view.setLayoutManager(layoutManager);
-        view.setAdapter(adapter);
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        int id = sharedPref.getInt("id", -1);
+
+        getBookInfo(id);
 
     }
-    public void fill(){
 
-        for(int i= 0 ; i< 12 ; i++) {
-            OldCommentsObj temp = new OldCommentsObj("bir kitab adı", "bir şetlre dsds c sd s d s cs s fd fs sdf sd fd fdvdfsdv d vds v dc svd v sdfsdsf fd sf ds fds fsdfds fd sf sdf sdf sd fsd fsd fsd fsd fsd f sdf sd fds fsd fsd dsf dsfsdf sdf sf sdf ds fsd fd sf sdf dsf ds fsd fsd fds fd sf sdf ds fsd", "12.12.2020", 3);
-            list.add(temp);
-        }
+    private void getBookInfo(final int id) {
+        String url = "http://18.204.251.116/user_comments.php";
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONArray jsonArray = new JSONArray(response);
+
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                String title = jsonArray.getJSONObject(i).getString("title");
+                                String commentText = jsonArray.getJSONObject(i).getString("review_text");
+                                String commentDate = jsonArray.getJSONObject(i).getString("review_date").split(" ")[0];
+                                String rate = jsonArray.getJSONObject(i).getString("rating");
+
+                                comment = new OldCommentsObj(title, commentText, commentDate, Integer.parseInt(rate));
+                                list.add(comment);
+                            }
+                                RecyclerView view = findViewById(R.id.old_comments);
+                                OldCommentAdapter adapter = new OldCommentAdapter(UserCommentsHistory.this, list);
+                                RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(UserCommentsHistory.this);
+                                view.setLayoutManager(layoutManager);
+                                view.setAdapter(adapter);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }){
+            @Override
+            protected Map<String, String> getParams() {
+                HashMap<String, String> params = new HashMap<>();
+                params.put("id", Integer.toString(id));
+                return params;
+            }
+        };
+        NetworkRequests.getInstance(this).addToRequestQueue(stringRequest);
     }
-
 
 }
